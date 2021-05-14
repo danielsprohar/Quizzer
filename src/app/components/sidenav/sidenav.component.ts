2c3c48f65dfe945ea68c9ac96d51825074f28a88
Daniel Sprohar <daniel.sprohar@outlook.com>
2021-05-14 08:06:03 -0500

    Changes: refactor the Sidenav and render the user's display name

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import firebase from 'firebase/app';
+import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { AppThemeService } from 'src/app/services/app-theme.service';
 
@@ -10,28 +10,17 @@ import { AppThemeService } from 'src/app/services/app-theme.service';
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent implements OnInit, OnDestroy {
-  private subscription: Subscription;
-
-  isUserSignedIn: boolean = false;
+export class SidenavComponent implements OnInit {
+  user$: Observable<firebase.User | null>;
 
   constructor(
-    private readonly afAuth: AngularFireAuth,
     private readonly router: Router,
-    public readonly authService: AuthService,
+    public readonly auth: AuthService,
     public readonly theme: AppThemeService
   ) {}
 
   ngOnInit(): void {
-    this.subscription = this.authService.isUserSignedIn().subscribe((value) => {
-      this.isUserSignedIn = value;
-    });
-  }
-
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
+    this.user$ = this.auth.getCurrentUser();
   }
 
   // =========================================================================
@@ -43,7 +32,7 @@ export class SidenavComponent implements OnInit, OnDestroy {
   }
 
   signOut(): void {
-    this.afAuth.signOut();
+    this.auth.signOut();
     this.router.navigate(['/']);
   }
 }
